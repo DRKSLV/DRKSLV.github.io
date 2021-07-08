0eeb04bdde2a88edd87f03cb19292b547705bed8
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2021-07-08 15:10:56 +0200

    Deploy

---
+++ assets/collapseList.js
@@ -0,0 +1,89 @@
+window.addEventListener("load", () => {
+    //collapseList
+    initCollapseList(".articleList");
+
+    // SIDEBAR
+    initSidebar();
+});
+
+// MODULES (copy paste because fuck webpack)
+//M: COLLAPSE LIST
+/**
+ * This function takes dom Measurements!
+ * Make sure content is visible and &lt;li&gt; tags define the "open" property as "true", "false" or "nochild".
+ * Use with collapseList.sass.
+ * @param {*} selector root selector for collapselist
+ */
+function initCollapseList(selector = ".collapseList") {
+    document.querySelectorAll(selector+" ul").forEach((node) => {
+        node.style.setProperty("--height", node.scrollHeight)
+    });
+    document.querySelectorAll(selector+" li").forEach((node) => {
+        node.classList.add("animOn");
+    });
+
+    document.querySelectorAll(selector+" li").forEach((node) => {
+        node.addEventListener("click", (evt) => {
+            var open = evt.target.getAttribute("open");
+
+            if(open!=="nochild")
+                setCollapse(evt.target, open==="true");
+            evt.stopPropagation();
+            return false;
+        })
+    });
+}
+
+function setCollapse(target, open) {
+    if(!open){
+        target.parentElement.childNodes.forEach((node) => {
+            if(node.tagName && node.tagName.toLowerCase() === "li") {
+                var open = node.getAttribute("open");
+                if(open!=="nochild") node.setAttribute("open", "false");
+            }
+        });
+    }
+
+    target.setAttribute("open", !open)
+}
+
+// SIDEBAR
+function initSidebar() {
+    const params = new URLSearchParams(window.location.search);
+
+    
+    var sidebarOpen = false;
+    /*
+    if (!sidebarOpen)
+    else
+        openSidebar();
+        */
+        closeSidebar();
+    
+
+    document.getElementById("mobileSidebarButton").addEventListener("click", (evt) => {
+        console.log("eeeee")
+        if(sidebarOpen) 
+            closeSidebar();
+        else 
+            openSidebar();
+
+        sidebarOpen = !sidebarOpen;
+    }, false);
+}
+
+function openSidebar() {
+    document.querySelectorAll(".sidebar").forEach((node) => {
+        node.classList.add("open");
+    });
+    var b = document.getElementById("mobileSidebarButton");
+    b.setAttribute("open", "true")
+}
+
+function closeSidebar() {
+    document.querySelectorAll(".sidebar").forEach((node) => {
+        node.classList.remove("open");
+    });
+    var b = document.getElementById("mobileSidebarButton");
+    b.setAttribute("open", "false")
+}
